5fb736394b202f7f993e04d7f8ab26530bd086c2
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-10-13 17:39:14 -0300

    CRUD Angular + Spring | 19: Formulário para Criar Cursos

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,11 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    _id: [''],
+    name: [''],
+    category: ['']
+  });
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location,
+    private route: ActivatedRoute) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
   }
 
+  ngOnInit(): void {
+    const course: Course = this.route.snapshot.data['course'];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    });
+  }
+
+  onSubmit(){
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
+  }
+
+  onCancel(){
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 });
+  }
 }
