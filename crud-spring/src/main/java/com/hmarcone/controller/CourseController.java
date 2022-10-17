9ee907e41e87dd61df6e0723496bb47252b5d345
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-10-17 16:26:31 -0300

    Update angular 12.2.0 para 13.3.11

--- crud-spring/src/main/java/com/hmarcone/controller/CourseController.java
+++ crud-spring/src/main/java/com/hmarcone/controller/CourseController.java
@@ -41,4 +41,27 @@ public class CourseController {
         // return ResponseEntity.status(HttpStatus.CREATED)
         // .body(courseRepository.save(course));
     }    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id,
+            @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }    
 }
