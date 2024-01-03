12fd0a6b16b1e82077cdf202b11d07c167199195
humberto.freitas <humberto.freitas@ionsistemas.com.br>
2024-01-03 17:18:38 -0300

    Aula 42 - Padrão Mapper

--- crud-spring/src/main/java/com/hmarcone/controller/CourseController.java
+++ crud-spring/src/main/java/com/hmarcone/controller/CourseController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import com.hmarcone.dto.CourseDTO;
 import com.hmarcone.model.Course;
 import com.hmarcone.service.CourseService;
 
@@ -34,24 +35,24 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<Course> list() {
+    public List<CourseDTO> list() {
         return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course) {
+    public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public Course update(@PathVariable @NotNull @Positive Long id,
-            @RequestBody @Valid Course course) {
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id,
+            @RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.update(id, course);
     }
 
