8d71555a71dcac0d090644b11fcf40e8db19f365
humberto.freitas <humberto.freitas@ionsistemas.com.br>
2024-01-03 16:00:28 -0300

    Removendo referencia

--- crud-spring/src/main/java/com/hmarcone/service/CourseService.java
+++ crud-spring/src/main/java/com/hmarcone/service/CourseService.java
@@ -1,7 +1,6 @@
 package com.hmarcone.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
